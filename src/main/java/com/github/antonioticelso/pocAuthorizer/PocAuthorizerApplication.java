e0c11dcd8b03ab9a33b152eef878a8af3dc007ba
ACPrado <antonio.ti.celso@gmail.com>
2024-09-21 17:27:30 +0000

    first commit

---
+++ src/main/java/com/github/antonioticelso/pocAuthorizer/PocAuthorizerApplication.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.pocAuthorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocAuthorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocAuthorizerApplication.class, args);
+	}
+
+}
